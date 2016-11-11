3d53b84f95563893b530725a962d32e430df9bbf
BrodyEscalera <brody.escalera@fullsail.edu>
2016-11-10 16:00:41 -0800

    working but only with hard coded id

--- test/__api.js
+++ test/__api.js
@@ -5,19 +5,13 @@
 const dotenv = require('dotenv').config();
 const request = require('supertest');// this reuires the supertest module and sets to a variable
 const chai = require('chai');
-const db = require('../src/models/db')
-const async = require('async')
+const db = require('../src/models/db');
+// const async = require('async')
 const expect = chai.expect;
 let testObjectId;
-
-let endPointsArray
-
-const testData = { link: 'www.testlink.com', testId:'MOD'}
-  getDynamicObjectId();
-
-
+const testData = { link: 'www.testlink.com', testId: 'MOD' };
 function getDynamicObjectId() {
-  let urlrouteConcatinated
+  let urlrouteConcatinated;
   db.url.findOrCreate({
     where: {
       link: 'localhost:3000/api/v3/url',
@@ -25,119 +19,100 @@ function getDynamicObjectId() {
       testId: 'TEST',
     },
   })
-   .spread(function(url, created) {
+   .spread((url) => {
      testObjectId = (url.get({
-       plain: true
-     }).id)
+       plain: true,
+     }).id);
 
+     urlrouteConcatinated = ('/api/v3/url/' + testObjectId);
+   });
 
-     urlrouteConcatinated =  ('/api/v3/url/'+ testObjectId);
-     console.log('inside',urlrouteConcatinated)
-
-  })
-  endPointsArray = [
-
-    {
-      testName: '/ should return full json database',
-      endPointRoute: '/api/v3/urls',
-      acceptedDataType:'application/json',
-      requestType: 'getAll',
-      statusCode: 200,
-      expectedProperties: ['id', 'link', 'shortUrl'],
-      expectedContentType: /json/,
-    },
-
-    {
-      testName: '/url should return the index.js html page',
-      endPointRoute: '/api/v3/url',
-      requestType: 'get',
-      acceptedDataType: 'text/html',
-      statusCode: 200,
-      expectedContentType: 'text/html; charset=UTF-8',
-    },
-
-    {
-      testName: '/ should return redirected to link html',
-      endPointRoute: '/api/v3/url/go/min.test',
-      acceptedDataType: 'text/html',
-      requestType: 'get',
-      statusCode: 200,
-      // expectedProperties: '',
-      expectedContentType: 'text/html; charset=utf-8',
-    },
-    {
-      testName: '/ should post test object to the url generated in the test',
-      endPointRoute: urlrouteConcatinated,
-      acceptedDataType:'application/json',
-      requestType: 'post',
-      statusCode: 200,
-      expectedProperties: ['id', 'link', 'shortUrl'],
-      postMessage: testData,
-      expectedContentType: 'application/json; charset=utf-8',
-    },
-    {
-      testName: '/ should return specified database object by :id with porperties of id, link , shorturl',
-      endPointRoute: urlrouteConcatinated,
-      acceptedDataType:'application/json',
-      requestType: 'get',
-      statusCode: 200,
-      expectedProperties: ['id', 'link', 'shortUrl'],
-      expectedContentType: /json/,
-    },
-
-  // this last item is for the delete function need to refactor to use a test object generated here.
-    // {
-    //   testName: '/ should return delete from the database to the url',
-    //   endPointRoute: '/api/v3/url/' + testObjectId,
-    //   requestType: 'delete',
-    //   expectedProperties: ['id', 'shortUrl'],
-    //   postMessage: '',
-    //   expectedContentType: 'application/json; charset=utf-8',
-    // },
-  ];
-console.log('insideAfter',urlrouteConcatinated)
 }
 
 describe('API route Tests', () => { // this runs through each available route within the server and tests the output.
-
   let server; // imports the server model.
-  // before(()=>{
-  //   // this.timeout(5000);
-  // })
-  // let testObjectId;
-  // let urlrouteConcatinated;
-
-
-console.log(endPointsArray)
   beforeEach(() => { // opens the server for each route and then closes.
-
     server = require('../src/server')
-        // db.url.create({
-        //   link: 'localhost:3000/api/v3/url',
-        //   shortUrl: 'min.test',
-        //   testId: 'TEST',
-        // });
-
+    getDynamicObjectId();
    });
 
    afterEach(() => {
 
-            // db.url.destroy({
-            //   where: {
-            //     testId: 'gEST',
-            //   },
-            // });
-            // db.url.destroy({
-            //   where: {
-            //     link: null,
-            //   },
-            // });
+            db.url.destroy({
+              where: {
+                testId: 'TEST',
+              },
+            });
+            db.url.destroy({
+              where: {
+                link: null,
+              },
+            });
 
-            // getDynamicObjectId();
+            getDynamicObjectId();
 
             server.close();
    });
+   const endPointsArray = [
 
+     {
+       testName: '/ should return full json database',
+       endPointRoute: '/api/v3/urls',
+       acceptedDataType: 'application/json',
+       requestType: 'getAll',
+       statusCode: 200,
+       expectedProperties: ['id', 'link', 'shortUrl'],
+       expectedContentType: /json/,
+     },
+
+     {
+       testName: '/url should return the index.js html page',
+       endPointRoute: '/api/v3/url',
+       requestType: 'get',
+       acceptedDataType: 'text/html',
+       statusCode: 200,
+       expectedContentType: 'text/html; charset=UTF-8',
+     },
+
+     {
+       testName: '/ should return redirected to link html',
+       endPointRoute: '/api/v3/url/go/min.test',
+       acceptedDataType: 'text/html',
+       requestType: 'get',
+       statusCode: 200,
+       // expectedProperties: '',
+       expectedContentType: 'text/html; charset=utf-8',
+     },
+     {
+       testName: '/ should post test object to the url generated in the test',
+       endPointRoute: '/api/v3/url/109',
+       acceptedDataType: 'application/json',
+       requestType: 'post',
+       statusCode: 200,
+       expectedProperties: ['id', 'link', 'shortUrl'],
+       postMessage: testData,
+       expectedContentType: 'application/json; charset=utf-8',
+     },
+     {
+       testName: '/ should return specified database object by :id with porperties of id, link , shorturl',
+       endPointRoute: '/api/v3/url/109',
+       acceptedDataType: 'application/json',
+       requestType: 'get',
+       statusCode: 200,
+       expectedProperties: ['id', 'link', 'shortUrl'],
+       expectedContentType: /json/,
+     },
+
+   // this last item is for the delete function need to refactor to use a test object generated here.
+     // {
+     //   testName: '/ should return delete from the database to the url',
+     //   endPointRoute: '/api/v3/url/' + testObjectId,
+     //   requestType: 'delete',
+     //   expectedProperties: ['id', 'shortUrl'],
+     //   postMessage: '',
+     //   expectedContentType: 'application/json; charset=utf-8',
+     // },
+   ];
   for (const routeEndPointIndex in endPointsArray) {
 
     it(endPointsArray[routeEndPointIndex].testName, (done) => { // specific tests
